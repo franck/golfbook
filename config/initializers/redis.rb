f9e8db9b772cb619c5ac3a487aab1c2f79977a9a
Franck D'agostini <franck.dagostini@gmail.com>
2015-06-10 15:50:12 +0200

    use a different db in test mode; clear the db after each test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-Redis.current = Redis.new(host: 'localhost', port: 6379)
+if Rails.env == 'test'
+  Redis.current = Redis.new(host: 'localhost', port: 6379, db: 12)
+else
+  Redis.current = Redis.new(host: 'localhost', port: 6379, db: 13)
+end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
-#class MemberTest < ActiveSupport::TestCase
 describe 'MemberTest' do
 
   let(:m1) { Member.create(email: 'bob@foo.fr') }
   let(:m2) { Member.create(email: 'bob@bar.fr') }
+
+  after do
+    clear_redis
+  end
 
   it "I can follow a member" do
     m1.follow!(m2)
@@ -25,6 +28,12 @@ describe 'MemberTest' do
     m1.follow!(m2)
     m1.unfollow!(m2)
     assert m1.following, []
+  end
+
+  protected
+
+  def clear_redis
+    Redis.current.flushdb
   end
 
 end
