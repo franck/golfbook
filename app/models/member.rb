29053a2b9a1f4488d7524f4dc27f529d2bab2bdc
Franck D'agostini <franck.dagostini@gmail.com>
2015-06-10 15:26:35 +0200

    adding member.follow, member.unfollow, member.followers, members.following

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,37 @@
 class Member < ActiveRecord::Base
+
+  def follow!(member)
+    redis.multi do
+      redis.sadd(self.redis_key(:following), member.id)
+      redis.sadd(member.redis_key(:followers), self.id)
+    end
+  end
+
+  def unfollow!(member)
+    redis.multi do
+      redis.srem(self.redis_key(:following), member.id)
+      redis.srem(member.redis_key(:followers), self.id)
+    end
+  end
+
+  def followers
+    member_ids = redis.smembers(redis_key(:followers))
+    Member.where(id: member_ids)
+  end
+
+  def following
+    member_ids = redis.smembers(redis_key(:following))
+    Member.where(id: member_ids)
+  end
+
+  protected
+
+  def redis
+    Redis.current
+  end
+
+  def redis_key(str)
+    "member:#{self.id}:#{str}"
+  end
+
 end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
-class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+#class MemberTest < ActiveSupport::TestCase
+describe 'MemberTest' do
+
+  let(:m1) { Member.create(email: 'bob@foo.fr') }
+  let(:m2) { Member.create(email: 'bob@bar.fr') }
+
+  it "I can follow a member" do
+    m1.follow!(m2)
+    assert_includes m2.followers, m1
+  end
+
+  it "I can see my followers" do
+    m1.follow!(m2)
+    assert m2.followers, [m1]
+  end
+
+  it "I can see members I follow" do
+    m1.follow!(m2)
+    assert m1.followers, [m2]
+  end
+
+  it "I can unfollow a member" do
+    m1.follow!(m2)
+    m1.unfollow!(m2)
+    assert m1.following, []
+  end
+
 end
